017ebe195637b1615682fb26ad0cee579aeb6ef4
patodekaz <pato@dekaz.cl>
2020-08-30 22:04:20 -0400

    apply config file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+
+  //NODE ENV
+
+  const serverConfig = config.get('server');
+  console.log(serverConfig.port);
+  
+
+  const port =  process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log('Application Listening on port ' + port);
 }
